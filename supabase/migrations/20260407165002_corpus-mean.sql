9138369f4e67fe27d0147282603bd5889ca3a24d
Eric Olaru-Hagen <e.olaruhagen@gmail.com>
2026-04-08 12:06:45 -0700

    migration to enable RLS security on new public tables (read only for anon) and corpus mean table (used for mean reduction of embeddings on the enrichment step soon)

---
+++ supabase/migrations/20260407165002_corpus-mean.sql
@@ -0,0 +1,42 @@
+-- Table used to store the mean embedding of the corpus of a certain artifact type: used during enrichment step when generating embedding
+-- Must use the embedding mean and mean reduce the artifacts embeddings to get the nuanced embedding for that artifact
+CREATE TABLE IF NOT EXISTS pipelines.corpus_mean (
+    id SERIAL PRIMARY KEY,
+    embedding extensions.halfvec(1536) NOT NULL,
+    count BIGINT NOT NULL
+);
+
+
+-- enable row level security for all tables in publich schema (artifacts, artifact_enrichment, article_details, stories) (read only for anon)
+
+ALTER TABLE "public"."artifacts" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "anon can read artifacts"
+  ON "public"."artifacts"
+  FOR SELECT
+  TO anon
+  USING (true);
+
+ALTER TABLE "public"."artifact_enrichment" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "anon can read artifact_enrichment"
+  ON "public"."artifact_enrichment"
+  FOR SELECT
+  TO anon
+  USING (true);
+
+ALTER TABLE "public"."article_details" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "anon can read article_details"
+  ON "public"."article_details"
+  FOR SELECT
+  TO anon
+  USING (true);
+
+ALTER TABLE "public"."stories" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "anon can read stories"
+  ON "public"."stories"
+  FOR SELECT
+  TO anon
+  USING (true);
